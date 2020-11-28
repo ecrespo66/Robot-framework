4da5b00dc9e6d53dc71a8faec66684f80b006ad6
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-11-28 21:21:28 +0100

    first version of robot template

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import robot
 
 
 if __name__ == "__main__":
-
-   print(json.loads(json.dumps(sys.argv[1][0])))
+   print(sys.argv[1:][0])
+   print(json.loads(json.dumps(sys.argv[1:][0])))
    #robot.Main(sys.argv[1][0]).runRobot()
