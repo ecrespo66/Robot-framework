e70aeb1d9ac6636ba11326cf875f76b198d89fd2
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-11-28 20:40:57 +0100

    first version of robot template

--- robot.py
+++ robot.py
@@ -4,7 +4,7 @@ import json
 
 class Main(Robot):
     def __init__(self, args):
-        self.params = json.parse(args)
+        self.params = json.loads(args)
         print(self.params)
         super().__init__(self.params['RobotId'], self.params['url'], self.params['ip'], self.params['port'],
                          self.params['params'], self.params['username'], self.params['password'])
