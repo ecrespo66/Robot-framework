24cb66604a30db0db0180983f1937071ac88e405
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-12-01 15:41:44 +0100

    first version of robot template

--- robot.py
+++ robot.py
@@ -1,20 +1,21 @@
+import time
+
 from iBot import Robot
 import json
 
 
 class Main(Robot):
     def __init__(self, args):
-
         self.params = args
-        super().__init__(robotId=self.params['RobotId'], ExecutionId= self.params['ExecutionId'],url= self.params['url'],
-                         ip=self.params['ip'], port=self.params['port'], username= self.params['username'],
-                         password=self.params['password'],params =self.params['params'])
+        super().__init__(robotId=self.params['RobotId'], ExecutionId=self.params['ExecutionId'], url=self.params['url'],
+                         ip=self.params['ip'], port=self.params['port'], username=self.params['username'],
+                         password=self.params['password'], params=self.params['params'])
+
     def runRobot(self):
         self.queue = self.createQueue("pollon")
-        self.Log.info("Info")
-        self.Log.systemException("System Exception")
-        self.Log.businessException("Business Exception")
-        self.Log.log("Log")
-
-
-
+        for i in range(0,60):
+            time.sleep(1)
+            self.Log.info("Info")
+            self.Log.systemException("System Exception")
+            self.Log.businessException("Business Exception")
+            self.Log.log("Log")
